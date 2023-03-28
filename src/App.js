ffa440f7495c24591a46310a30b98e4bb4b80bf3
saimang12@naver.com <saimang12@naver.com>
2023-03-28 12:20:30 +0900

    2023-03-28

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [data, setData] = useState([]);
+    const [input, setInput] = useState('');
+    const [search, setSearch] = useState('man');
+    const [load, setLoad] = useState(true);
+
+    const pixaData = `https://pixabay.com/api/?key=34354439-e2d66846314cd63eaa92b43df&q=${search}&image_type=photo&page=2&per_page=200&lang=ko`;
+    const getData = async () => {
+        setLoad(true);
+        const r = await fetch(pixaData).then(it => it.json());
+        setData(r.hits);
+        setLoad(false);
+    }
+    useEffect(() => { getData() }, [search]);
+
+    const onSubmit = e => {
+        e.preventDefault();
+        // if (input.length < 3) {
+        //     alert('더 입력하세요')
+        //     return
+        // }
+        setSearch(input);
+    }
+    const onChange = e => setInput(e.target.value);
+
+    console.log(data);
+
+    if (load) {
+        return <div className="load"><i class="xi-spinner-5 xi-spin"></i></div>
+    }
+    return (
+        <div className="app">
+            <form onSubmit={onSubmit}>
+                <input type="text" onChange={onChange} />
+                <button><i className="xi-search"></i></button>
+            </form>
+            <ul className="list">
+                {
+                    data.map(it => {
+                        return (
+                            <li key={it.id}>
+                                <figure>
+                                    <img src={it.largeImageURL} alt="" />
+                                </figure>
+                                <strong>{it.tags}</strong>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+
+
+    )
+
+
+
+
 }
 
 export default App;
